79b8dd6daf3ef1cc0776a76cece77740bf4fbe59
Davide Zattra <d.zattra@gmail.com>
2021-11-15 15:43:20 +0100

    run seeder and modify model\post

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,20 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for( $i = 0; $i < 50; $i++ ){
+
+            $newPost = new Post();
+
+            $newPost->title = $faker->words(4, true);
+            $newPost->author = $faker->firstName().' '.$faker->lastName();
+            $newPost->content = $faker->paragraph(50, false);
+            $newPost->date = $faker->date('Y-m-d');
+            $newPost->img_url = $faker->imageUrl();
+
+            $newPost->save();
+
+        }
     }
 }
