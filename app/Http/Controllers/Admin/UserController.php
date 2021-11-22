ce75742a4fe66714d676543525adf1a3bf267f6a
Davide Zattra <d.zattra@gmail.com>
2021-11-22 17:21:02 +0100

    implemented delete

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -106,8 +106,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
