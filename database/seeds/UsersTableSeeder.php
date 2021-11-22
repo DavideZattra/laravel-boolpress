80945105d4a499ecfb50117b3159083435775bce
Davide Zattra <d.zattra@gmail.com>
2021-11-22 15:27:21 +0100

    modified user seeder to add roles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
 use Faker\Generator as Faker;
 
 use App\User;
+use App\Models\Role;
 
 
 class UsersTableSeeder extends Seeder
@@ -15,11 +18,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $roles_id = Role::pluck('id')->toArray();
+
         $admin = new User();
         $admin->name = 'Davide';
         $admin->email = 'test@test.com';
         $admin->password = bcrypt('password');
         $admin->save();
+        $admin->roles()->attach(1);
 
         /**
          * For cicle to add 9 new users
@@ -31,6 +37,14 @@ class UsersTableSeeder extends Seeder
             $newUser->email = $faker->safeEmail();
             $newUser->password = bcrypt($faker->password(8, 15));
             $newUser->save();
+
+            //random number of tags added
+            for($j = 0; $j < random_int(0, count($roles_id)); $j++){
+
+                $role = Arr::random($roles_id);
+    
+                $newUser->roles()->attach($role);
+            }
         }
     }   
 }
